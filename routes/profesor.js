11fc32c64e9a091dd6899fc3535354ff8b35b281
Eduardo Hortiguela <eduardohortiguela@gmail.com>
2021-01-05 17:47:04 +0100

    login in api

--- routes/profesor.js
+++ routes/profesor.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Prof_model = require('../models/Profesor_model.js');
 const bcrypt = require("bcryptjs");
 
-/* GET users listing. */
+/* GET profesor listing. */
 router.get('/', function(req, res, next) {
   Prof_model.find().sort('nombre').exec(function(err,profesorInfo){
       if(err)res.status(500).send(err);
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-/* GET users for id. */
+/* GET profesor for id. */
 router.get('/:id', function(req, res, next) {
   Prof_model.findById(req.params.id,function(err,profesorInfo){
     if(err) res.status(500).send(err);
@@ -21,7 +21,7 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-/* post alumno. */
+/* post profesor. */
 router.post('/', function(req, res, next) {
     req.body.pass= bcrypt.hashSync(req.body.pass,10)
    Prof_model.create(req.body, function(err,profesorInfo){
@@ -30,7 +30,7 @@ router.post('/', function(req, res, next) {
  });
 });
 
-/* put alumno por id. */
+/* put profesor por id. */
 router.put('/:dni', function(req, res,next){
   Prof_model.findOneAndUpdate({dni:req.params.dni},req.body,function(err,profesorInfo){
     if(err)res.status(500).send(err);
@@ -38,12 +38,40 @@ router.put('/:dni', function(req, res,next){
   });
 });
 
-/* delete alumno por dni */
+/* delete profesor por dni */
 router.delete('/:id', function(req, res, next) {
   Prof_model.findByIdAndDelete(req.params.id,function(err,profesorInfo){
     if(err) res.status(500).send(err);
     else res.sendStatus(200);
   });
+});
+
+/* loguin por profesor*/
+
+router.post('/loguin', async(req,res)=>{
+  const profesor = await Prof_model.findOne({dni: req.body.dni});
+  
+  if(profesor){
+    
+    const iguales = bcrypt.compareSync(req.body.pass, profesor.pass);
+     
+    if(iguales){
+      
+      res.json({error: 'ok log'});
+
+    }else{
+
+      res.json({error: 'error 1'});
+    }
+
+  }else{
+    res.json({error: ver});
+    
+    
+    
+  }
+ 
+ 
 });
 
 
