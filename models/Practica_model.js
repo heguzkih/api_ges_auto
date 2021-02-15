01236d355a525d9d73f019fe720b4ad2b6e3f71c
Eduardo Hortiguela <eduardohortiguela@gmail.com>
2021-02-16 00:34:12 +0100

    implemetcaion hora inicio fin

--- models/Practica_model.js
+++ models/Practica_model.js
@@ -5,7 +5,8 @@ var PracticaSchema = mongoose.Schema({
     //id: { type: ObjectId, required: true },
     profesor:{type: mongoose.Schema.Types.ObjectId, ref:'profesor',autopopulate:true},
     alumno:{type: mongoose.Schema.Types.ObjectId, ref:'alumnos', autopopulate: true},
-    fecha:{type: Date, default:Date.now },
+    fechaFin:{type: Date, default:Date.now},
+    fechaInicio:{type: Date, default:Date.now },
     permiso:String,
 });
 PracticaSchema.plugin(require('mongoose-autopopulate'));
